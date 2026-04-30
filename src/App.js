a78f89374db705c14d50f2b1d79bfb172ae2b4b5
telemaster <telemasterreports@example.com>
2026-05-01 03:00:27 +0530

    package.json file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AgentReport from "./page/AgentReport";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
+import AgentReport from "./page/AgentReport";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/agent-report" element={<AgentReport />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
+<Router basename="/Telezone">
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/agent-report" element={<AgentReport />} />
+  </Routes>
+</Router>
