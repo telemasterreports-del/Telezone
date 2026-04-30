765089d2e9bd02ff6978bc7e8242db699a627017
telemaster <telemasterreports@example.com>
2026-05-01 03:15:35 +0530

    removed git homepage service

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import AgentReport from "./page/AgentReport";
 
-function App() {
-  return (      
 
-<Router basename="/Telezone">
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/agent-report" element={<AgentReport />} />
-  </Routes>
-</Router> 
+function App() {
+  return (
+    <Router >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/agent-report" element={<AgentReport />} />
+      </Routes>
+    </Router>
   );
 }
 
